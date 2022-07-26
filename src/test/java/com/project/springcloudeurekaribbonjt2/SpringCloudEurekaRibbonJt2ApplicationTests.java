dcf06887e5d7830a3fc72aef77a724dd458ce9e2
Emerson Millard <emillard.code@gmail.com>
2022-07-26 12:00:00 -0700

    Modified test class.

--- src/test/java/com/project/springcloudeurekaribbonjt2/SpringCloudEurekaRibbonJt2ApplicationTests.java
+++ src/test/java/com/project/springcloudeurekaribbonjt2/SpringCloudEurekaRibbonJt2ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.springcloudeurekaribbonjt2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCloudEurekaRibbonJt2ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
