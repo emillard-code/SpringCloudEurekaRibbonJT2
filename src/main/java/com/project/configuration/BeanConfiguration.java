83172e76f5bf75cf2fc6edb3043a2bbaf21799a2
Emerson Millard <emillard.code@gmail.com>
2022-07-27 12:00:00 -0700

    Added configuration class.

---
+++ src/main/java/com/project/configuration/BeanConfiguration.java
@@ -0,0 +1,19 @@
+package com.project.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate template() {
+
+        return new RestTemplate();
+
+    }
+
+}
